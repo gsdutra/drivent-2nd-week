aa5b49f84c4e516f1ddfc601c8434eff45c4b881
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-04-27 14:47:38 -0300

    chore: create test descriptions

--- tests/integration/bookings.test.ts
+++ tests/integration/bookings.test.ts
@@ -38,7 +38,107 @@ describe('GET /booking', () => {
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
 
-  //   describe('When token is valid', ()=>{
-  // 	it('should return')
-  //   })
+  describe('When token is valid', () => {
+    it('should return status 404 if user does not have a booking', () => {
+      0;
+    });
+
+    it('should return status 200 with booking data on success', () => {
+      0;
+    });
+  });
+});
+
+describe('POST /booking', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.get('/booking');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await factory.createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('When token is valid', () => {
+    it('should return status 403 if ticket is remote', () => {
+      0;
+    });
+
+    it('should return status 403 if ticket does not include hotel', () => {
+      0;
+    });
+
+    it('should return status 403 if ticket has not been paid yet', () => {
+      0;
+    });
+
+    it('should return status 403 if the room has no vacancies', () => {
+      0;
+    });
+
+    it('should return status 404 if the room does not exist', () => {
+      0;
+    });
+
+    it('should return status 200 with booking ID on success', () => {
+      0;
+    });
+  });
+});
+
+describe('PUT /booking', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.get('/booking');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await factory.createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('When token is valid', () => {
+    it('should return status 403 if user does not have a booking', () => {
+      0;
+    });
+
+    it('should return status 403 if the new room has no vacancies', () => {
+      0;
+    });
+
+    it('should return status 404 if the room does not exist', () => {
+      0;
+    });
+
+    it('should return status 200 with booking ID on success', () => {
+      0;
+    });
+  });
 });
